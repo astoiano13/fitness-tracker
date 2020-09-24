df2e22abc7b27ae62a23599dff4a44232fd69e80
Aaron Stoiano <aaronstoiano@gmail.com>
2020-09-23 22:53:30 -0400

    added get routes and connceted mongoose

---
+++ server.js
@@ -0,0 +1,37 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var morgan = require("morgan");
+
+var app = express();
+app.use(morgan("dev"))
+
+var PORT = process.env.PORT || 3000;
+
+var user = require("./models")
+
+app.use(express.json());
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+// ==================================
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+
+app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "exercise.html"));
+  });
+
+app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "stats.html"));
+  });
+
+// ==================================
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+  });
